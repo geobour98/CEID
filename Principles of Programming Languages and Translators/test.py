69f4d8f6601b82d1c29afa26c8038781e933e307
George Bourlakis <giorgosbourl@gmail.com>
2021-11-14 12:48:31 +0200

    Add files via upload

---
+++ Principles of Programming Languages and Translators/test.py
@@ -0,0 +1,32 @@
+import math
+#test%
+
+dict1 = {("car", "blue"), ("bike", "black")}
+dict1.items()
+dict1.setdefault("car", "white")
+class myclass1:
+	
+	def fun(self, a):
+		a = 3
+		print("self.a=",a)
+		return a
+		
+
+def fun2():
+	d = 4
+	return d
+	
+a = lambda a: a+2
+print("check:",a)
+
+age = input("Dwse thn hlikia sou: ")
+
+list1 = [1,2,3,4.5,"String"]
+for x in list1:
+	print(x)
+
+b = 1
+c = 2
+if b > a:
+	print("To b einai megalytero tou a")
+	
