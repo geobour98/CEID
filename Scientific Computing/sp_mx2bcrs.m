167dcce327c1ac2d09686ff770be6f14b3183389
George Bourlakis <giorgosbourl@gmail.com>
2021-11-14 15:17:08 +0200

    Add files via upload

---
+++ Scientific Computing/sp_mx2bcrs.m
@@ -0,0 +1,34 @@
+function [val,col_idx,row_blk] = sp_mx2bcrs(A,nb)
+% Author: ?. ??????????, ?? 1054321, Date 13/2/2021
+
+[m,n] = size(A);
+if m~=n; error('Oi diastaseis tou mhtrwou den einai idies!'); end
+
+val = [];
+col_idx = [];
+row_blk = [];
+
+counter = 1:m/nb;
+cnt = 0;
+step = nb;
+
+for i=1:step:m 
+    i1 = 0;
+    flag = 1;
+    for j=1:step:n
+        i1 = i1+1;    
+        blk = A(i:i+step-1,j:j+step-1);
+        if nnz(blk) >= 1
+            val = [val blk]; 
+            col_idx = [col_idx counter(i1)];            
+            cnt = cnt+1;
+            if flag == 1
+                row_blk = [row_blk cnt];
+            end
+            flag = 0;
+        end
+    end
+end
+row_blk = [row_blk cnt+1];
+
+end
---
+++ Scientific Computing/spmv_bcrs.m
@@ -0,0 +1,18 @@
+function y = spmv_bcrs(y,val,col_idx,row_blk,x)
+% Author: ?. ??????????, ?? 1054321, Date 14/2/2021
+
+[m,n] = size(val);
+ 
+for i=1:length(row_blk)-1    
+    i1 = (row_blk(i)*m)-1; 
+    i2 = (row_blk(i+1)*m)-2;
+    item = [];
+    cnt = (i2-i1+1)/m;
+    for j=1:cnt 
+        item = [item; x(col_idx(j)*m-1:col_idx(j)*m+m-2)];
+    end
+    col_idx = col_idx(cnt+1:end);
+    y(i*m-m+1:i*m) = y(i*m-m+1:i*m)+val(:,i1:i2)*item;
+end
+ 
+end
